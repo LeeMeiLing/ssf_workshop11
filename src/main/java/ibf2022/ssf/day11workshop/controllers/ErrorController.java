69f7a43dff63cd8c35f98410e2cb489f68eb28c1
LeeMeiLing <leem0060@outlook.com>
2023-02-28 18:16:16 +0800

    added @ControllerAdvice in error controller to access error attributes

--- src/main/java/ibf2022/ssf/day11workshop/controllers/ErrorController.java
+++ src/main/java/ibf2022/ssf/day11workshop/controllers/ErrorController.java
@@ -1,18 +1,18 @@
 package ibf2022.ssf.day11workshop.controllers;
 
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+
+@ControllerAdvice
 @RequestMapping(path = {"/error"}, produces = MediaType.TEXT_HTML_VALUE)
 public class ErrorController {
-    
 
     @GetMapping
     public String getError(){  
-        return "error";
+       return "error";
 
     }
 
